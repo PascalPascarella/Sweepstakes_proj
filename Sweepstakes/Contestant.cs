77ab0081d3085af02965da75dd34ac276de0e6f0
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-10-07 08:35:37 -0500

    Build initial Contestant

--- Sweepstakes/Contestant.cs
+++ Sweepstakes/Contestant.cs
@@ -6,5 +6,19 @@ namespace Sweepstakes
 {
 	class Contestant
 	{
+		// Member Variables
+		public string FirstName;
+		public string LastName;
+		public string EmailAddress;
+		public int RegistrationNumber;
+
+		// Constructor
+		public Contestant(string FirstName, string LastName, string EmailAddress, int RegistrationNumber)
+		{
+			this.FirstName = FirstName;
+			this.LastName = LastName;
+			this.EmailAddress = EmailAddress;
+			this.RegistrationNumber = RegistrationNumber;
+		}
 	}
 }
