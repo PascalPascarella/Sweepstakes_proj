5078ff1c51b918ef7fd0211f856a673f6fe49045
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-10-08 02:44:26 -0500

    Remove unused brackets from ln 9 method, Insert functionality comments

--- Sweepstakes/ISweepstakesManager.cs
+++ Sweepstakes/ISweepstakesManager.cs
@@ -6,10 +6,7 @@ namespace Sweepstakes
 {
 	interface ISweepstakesManager
 	{
-		void InsertSweepstakes(Sweepstakes sweepstakes)
-		{
-
-		}
-		Sweepstakes GetSweepstakes();
+		void InsertSweepstakes(Sweepstakes sweepstakes);		// Will be used to push/enqueue the sweepstakes onto stack/queue
+		Sweepstakes GetSweepstakes();   // Will be used to pop/dequeue the sweepstakes from stack/queue
 	}
 }
