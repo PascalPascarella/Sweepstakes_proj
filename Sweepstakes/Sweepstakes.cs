f784f7e37059b7187ea5493c8e63b6a39840dcda
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-10-08 02:30:44 -0500

    Build PickWinner method ln 32

--- Sweepstakes/Sweepstakes.cs
+++ Sweepstakes/Sweepstakes.cs
@@ -10,26 +10,48 @@ namespace Sweepstakes
 		private Dictionary<int, Contestant> contestants;
 		private string name;
 		public string Name;
+		public Contestant winnerContest;
 
 		// Constructor
 		public Sweepstakes(string name)
 		{
 			this.name = name;
+			contestants = new Dictionary<int, Contestant>();
 		}
 
 		// Member Methods
-		public void RegisterContestant(Contestant contestant)
+		public void RegisterContestant(Contestant contestant)		// Assigns contestant object an int Key and adds to Dictionary
 		{
+			int regisNumber = contestants.Count + 1;
+
+			contestants.Add(regisNumber, contestant);
+
+			contestant.RegistrationNumber = regisNumber;
 
 		}
 		public Contestant PickWinner()
 		{
-
-			return;
+			
+			int winnerKey = GetRandomNumber(contestants.Count + 1);
+			foreach (KeyValuePair<int, Contestant> winner in contestants)
+			{
+				if(winnerKey == winner.Key)
+				{
+					winnerContest = winner.Value;
+				}
+			}
+			return winnerContest;
 		}
-		public void PrintContestantInfo(Contestant contestant)
+	public void PrintContestantInfo(Contestant contestant)
 		{
 
 		}
+		public int GetRandomNumber(int max)
+		{
+			Random random = new Random();
+			int randomResult = random.Next(max);
+			return randomResult;
+		}
+
 	}
 }
