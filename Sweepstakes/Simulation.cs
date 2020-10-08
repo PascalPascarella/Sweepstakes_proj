14b00da4b94b12e6a3442f0cb0513c4b993f80ec
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-10-08 02:29:25 -0500

    Remove test conditions, Build out CreateMarketingFirmWithManager method

--- Sweepstakes/Simulation.cs
+++ Sweepstakes/Simulation.cs
@@ -6,5 +6,18 @@ namespace Sweepstakes
 {
 	class Simulation
 	{
+		// Member Variables
+
+
+		// Constructor
+		public Simulation()
+		{
+		}
+
+		// Member Methods
+		public void CreateMarketingFirmWithManager(ISweepstakesManager manager)
+		{
+			MarketingFirm marketingFirm1 = new MarketingFirm(manager);
+		}
 	}
 }
