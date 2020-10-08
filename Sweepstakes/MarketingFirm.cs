b29c1a06ed49d516e61ae383a83da3aaededac75
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-10-08 02:28:10 -0500

    Update CreateSweepstake method ln 22

--- Sweepstakes/MarketingFirm.cs
+++ Sweepstakes/MarketingFirm.cs
@@ -6,5 +6,21 @@ namespace Sweepstakes
 {
 	class MarketingFirm
 	{
+		// Member Variables
+		private ISweepstakesManager _manager;
+
+		// Constructor
+		public MarketingFirm(ISweepstakesManager manager)
+		{
+			_manager = manager;
+
+		}
+		// Member Methods
+		public void CreateSweepstake(string name)		// Factory??
+		{
+			// -->TODO: UI
+			Sweepstakes sweepstakes = new Sweepstakes(name);
+
+		}
 	}
 }
